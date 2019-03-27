27f1258c6c861b85137ad619478483572f71cf7f
Meghane Schifano <meghanesfn@MacBook-Pro-de-Meghane.local>
2019-03-27 18:51:18 +0100

    Devoir-codingskills

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  # get 'games/create'
+  # get 'games/edit'
+  # get 'games/update'
+  # get 'games/show'
+  resources :games
   # get 'pages/home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "pages#home"
